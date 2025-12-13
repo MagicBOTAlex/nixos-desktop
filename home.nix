77d67d319876dfc6ddec7c23f2eb460477c332c5
BOTAlex <alex@deprived.dev>
2025-12-13 15:57:30 +0100

    chinese font fixed

--- home.nix
+++ home.nix
@@ -14,9 +14,6 @@ in
   ] ++ lib.optional (toggles.vscode.enable) ./homeModules/vscode.nix
   ++ lib.optional (toggles.wezterm.enable or false) ./homeModules/wezterm.nix;
 
-  # packages only for this user
-  home.packages = [ ];
-
   # env variables for this user
   home.sessionVariables = {
     EDITOR = "nvim"; # use nvim as editor
@@ -32,5 +29,27 @@ in
     sha256 = "1xd5yn6czgym6ayp5bb9mj409a4d78839c6cjkky2l2264hc04q1";
   };
 
-  fonts.fontconfig.enable = true;
+  fonts.fontconfig = {
+    enable = true;
+    configFile."no-bitmaps" = {
+      enable = true;
+      priority = 50;
+      text = ''
+        <?xml version="1.0"?>
+        <!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
+        <fontconfig>
+          <match target="font">
+            <edit name="embeddedbitmap" mode="assign">
+              <bool>false</bool>
+            </edit>
+          </match>
+        </fontconfig>
+      '';
+    };
+  };
+  home.packages = with pkgs; [
+    noto-fonts-cjk-sans # Sans-serif Chinese fonts
+    noto-fonts-cjk-serif # Serif Chinese fonts
+    wqy_zenhei # Optional: A popular alternative if Noto doesn't work well
+  ];
 }
